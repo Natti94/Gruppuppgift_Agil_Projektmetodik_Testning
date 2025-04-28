1219e18b71079277eb265551ffa7d85eeabb2392
Natti94 <natnael.berhane.sv@gmail.com>
2025-04-28 15:31:34 +0200

    update

--- demo_vecka_3/vite.config.js
+++ demo_vecka_3/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./test/setup.js",
+    setupFiles: "../demo_vecka_3/src/test/setup.js",
   },
   server: {
     proxy: {
